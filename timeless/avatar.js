df27bd4b8cab6e1dc6ada89ce11ddb3fad33ecdb
Bagu <clemens.joe.gh@gmail.com>
2025-04-08 04:06:43 +0700

    Added lore and credits. FIxed avatar css
    - Codebase reached 2356 lines of code lmfaoo
    - Readded some css that I forgot to re-add

--- timeless/avatar.js
+++ timeless/avatar.js
@@ -1,92 +1,29 @@
 // avatar 1-4 (index 0-3)
 const avatars = [
     { src: "assets/characters/KEVIN.png", name: "Kevin", 
-        description: "Kevin is a brave warrior." },
+        description: `Kevin was once the Empire's most revered knight. He stood for the imperial will his entire life. 
+        However, this changed after the Purge of Ponorogo. Where the Imperial command demanded the execution of Ponorogo 
+        citizens. Among them were Kevin's ailed father and his younger sister. On the dawn of the execution day, 
+        the people watched as the Empire's perfect weapon fire upon his own family. \n\n
+        With a single sword draw, Kevin cuts cleanly with precision that branded him as the Empire's Perfect Weapon.\n
+        But it was not his family that he cut. It was the executioner's blade. Kevin decided to betray the Empire and 
+        escaped with his family, outpacing the Imperial Guards. Now, he wanders through cities, fighting against evil 
+        wherever he could despite the people he protect calling him a traitor.` },
     { src: "assets/characters/REGINA.png", name: "Regina", 
-        description: "Regina is a skilled archer." },
+        description: `Regina was a member of the Imperial Family. She is the daughter of the Emperor, Regina is skilled 
+        on archery, However, this became an irony as the emperor was assassinated by the Demon Queen's forces by a 
+        poisoned arrow. \n\n She vows to make things right by shooting the same arrow through the heart of the Demon Queen.
+        Recently, she has caught rumours of the queen's presence in Ponorogo. Will she reach her?` },
     { src: "assets/characters/LINA.png", name: "Lina", 
-        description: "Lina is a powerful mage." },
-    { src: "assets/characters/BASTIAN.png", name: "Bastian", // TODO: DELETE BEE SCRIPT ONCE PLACEHOLDER IS NO LONGER NEEDED
-        description: `根據所有已知的法律
-的航空，
-
-  
-有沒有辦法蜜蜂
-應該能飛。
-
-  
-它的翅膀太小了
-它的脂肪小身體離地。
-
-  
-蜜蜂，當然，蒼蠅無論如何
-
-  
-因為蜜蜂不在乎
-人類認為是不可能的。
-
-  
-黃色，黑色。黃色，黑色。
-黃色，黑色。黃色，黑色。
-
-  
-哦，黑色和黃色！
-讓我們搖一搖它。
-
-  
-巴里！早餐準備好了！
-
-  
-Ooming！
-
-  
-稍等一下。
-
-  
-你好？
-
-  
-- 巴里？
-- 亞當？
-
-  
-- 你相信這是發生嗎？
-- 我不能。我會接你。
-
-  
-看起來鋒利。
-
-  
-使用樓梯。你的父親
-付出好錢的那些。
-
-  
-抱歉。我很興奮。
-
-  
-這是畢業生。
-我們為你自豪，兒子。
-
-  
-完美的報告卡，所有B的。
-
-  
-很自豪。
-
-  
-嘛！我有一件事在這裡。
-
-  
-- 你的絨毛上有絨毛。
-- Ow！那是我！
-
-  
-- 波到我們！我們將在第118,000行。
-- 再見！
-
-  
-巴里，我告訴你，
-停止在房子裡飛行！` },
+        description: `Lina is a powerful mage within the Imperial Guards. She questioned the will of the Emperor her 
+        entire life. Despising the lavish life that the emperor enjoyed. She grew close to a knight called Kevin. As time 
+        goes on, Kevin and Lina became close friends. Lina was one of the key orchestrator of Kevin's escape with his family.
+        \n\n Her appearance might suggests otherwise, but she is one of the strongest mage on the continent. She recently 
+        hear a rumor of a Knight that travels from city through city. Fighting monsters. \n Hearing this, Lina decided to 
+        resign from her position and embark a journey to find a long lost friend.` },
+    { src: "assets/characters/BASTIAN.png", name: "Bastian",
+        description: `Never gonna give you up, never gonna let you down. Never gonna go around and desert you. 
+        Never gonna make you cry. Never gonna say goodbye~` }, // TODO: add lore, remove rickroll
 ];
 
 let currentAvatarIndex = 0; // current avatar
