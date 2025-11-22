4ab80fff2f84812275e5f668771edce297eff084
Baguette <clemens.joe.gh@gmail.com>
2025-11-22 20:22:40 +0700

    Updates

--- src/components/layout/Card.tsx
+++
@@ -1,7 +0,0 @@
-import "./Card.css";
-
-export const Card = ({ children }: { children: React.ReactNode }) => (
-  <div className="card-container">
-    {children}
-  </div>
-);
--- src/components/layout/Container.tsx
+++
@@ -1,5 +0,0 @@
-export const Container = ({ children }: { children: React.ReactNode }) => (
-  <div className="container">
-    {children}
-  </div>
-);
