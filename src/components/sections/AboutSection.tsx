8d08bb8be8b3c77727a622401c94e6b01ed8809a
Baguette <clemens.joe.gh@gmail.com>
2025-11-23 06:27:08 +0700

    credits & make coloring consistent for projects header

--- src/components/sections/AboutSection.tsx
+++ src/components/sections/AboutSection.tsx
@@ -13,10 +13,11 @@ export const AboutSection: React.FC = () => {
         incorporating new technologies and things I've learned along the way.
 
         The first version (<a href="https://github.com/Not-Baguette/not-baguette.github.io/commit/f737144bd701fe99367b34dceae84cca5b933199" target="_blank" rel="noopener noreferrer" className="version-link">V1</a>) was a simple HTML/CSS site, while the second (<a href="https://github.com/Not-Baguette/not-baguette.github.io/tree/v2-last-ver" target="_blank" rel="noopener noreferrer" className="version-link">V2</a>) introduced
-        proper Javascript for interactivity, design stuff such as parralax and animations.
-        This is where I started learning React as well (Seen with <a href="https://github.com/Not-Baguette/not-baguette.github.io/tree/v2-last-ver/timeless" target="_blank" rel="noopener noreferrer" className="version-link">timeless</a>).
-        <a href="https://github.com/Not-Baguette/not-baguette.github.io" target="_blank" rel="noopener noreferrer" className="version-link"> V3</a> is built with React and TypeScript, with actual API endpoints usage.
+        proper Javascript for interactivity, That's where I started learning React as well (Seen with <a href="https://github.com/Not-Baguette/not-baguette.github.io/tree/v2-last-ver/timeless" target="_blank" rel="noopener noreferrer" className="version-link">timeless</a>).
+        <a href="https://github.com/Not-Baguette/not-baguette.github.io" target="_blank" rel="noopener noreferrer" className="version-link"> V3</a> is built with React and TypeScript, with actual API endpoints usage :D
       </p>
+      
+      <p className="about-description">Thanks & Credits to Naomi and Eric for helping me with fixing the design & Font selection! ♡</p>
       <div className="obsessions-box">
         <strong className="obsessions-title">Current Themes:</strong><br/>
         • Pastel color palettes<br/>
